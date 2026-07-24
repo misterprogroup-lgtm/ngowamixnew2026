c4a224aeedd4d1b565731cae5a99715f6b0f77b0
Jeremie NGuessan <jeremienguessan@Jeremies-MacBook-Pro.local>
2026-07-24 01:38:23 +0000

    fix: add JwtModule import to MusicModule for stream token verification

--- apps/api/src/modules/music/music.module.ts
+++ apps/api/src/modules/music/music.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MusicService } from './music.service';
 import { MusicController } from './music.controller';
 import { RedisModule } from '../../common/modules/redis/redis.module';
 
 @Module({
-  imports: [RedisModule],
+  imports: [
+    RedisModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        secret: config.get<string>('JWT_SECRET'),
+      }),
+    }),
+  ],
   controllers: [MusicController],
   providers: [MusicService],
   exports: [MusicService],
